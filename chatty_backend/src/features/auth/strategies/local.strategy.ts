1fdef3171273111f9c01db839eb18027ac6ce280
Andrea Hsu <andrea.hsu1030@gmail.com>
2025-03-17 17:41:39 +0800

    fix: 修正錯誤帳密時的回覆

--- chatty_backend/src/features/auth/strategies/local.strategy.ts
+++ chatty_backend/src/features/auth/strategies/local.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthService } from '../auth.service';
@@ -14,7 +14,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     const user = await this.authService.validateUser(username, password);
 
     if (!user) {
-      throw new UnauthorizedException(username, password);
+      throw new NotFoundException();
     }
 
     const payload: IUserPayload = {
