828588f9e13f1c645c7a56b7b3eab26ef40e37c9
Andrea Hsu <andrea.hsu1030@gmail.com>
2025-03-18 13:08:40 +0800

    style: loading

---
+++ chatty_client/src/pages/loading.tsx
@@ -0,0 +1,9 @@
+// import icon from '/favicon'
+export const Loading = () => {
+  return (
+    <div className='fixed inset-0 flex items-center justify-center bg-white'>
+      <p className="flex items-center gap-2 text-gray-500"> <img src='favicon.png' /> Loading ..</p>
+    </div>
+  )
+}
+export default Loading
