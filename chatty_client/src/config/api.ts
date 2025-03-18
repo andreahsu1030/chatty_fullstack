6f463bf8b601c9ce3281216da399adb7ccdfc1aa
Andrea Hsu <andrea.hsu1030@gmail.com>
2025-03-18 13:09:10 +0800

    feat: 好友申請流程

--- chatty_client/src/config/api.ts
+++ chatty_client/src/config/api.ts
@@ -7,6 +7,9 @@ export const API_AUTH_POST_SIGNUP = 'http://localhost:3000/auth/signup'
 //Get User Profile
 export const API_PROFILE_GET_USER = 'http://localhost:3000/profiles/search'
 
+//blur search
+export const API_PROFILE_POST_PROFILES = 'http://localhost:3000/profiles/search'
+
 //create
 export const API_PROFILES_POST_CREATE = 'http://localhost:3000/profiles/create'
 
@@ -23,10 +26,12 @@ export const API_PROFILE_PATCH_CONTENT = 'http://localhost:3000/profiles'
 export const API_AUTH_GET_USER = 'http://localhost:3000/auth/me'
 
 //用登入者id拿好友ids
-export const API_FRIENDSHIPS_POST_SEARCH = 'http://localhost:3000/friendships/search'
+export const API_FRIENDSHIPS_POST_SEARCH =
+  'http://localhost:3000/friendships/search'
 
 //update 好友申請狀態
-export const API_FRIENDSHIPS_PATCH_STATUS = 'http://localhost:3000/friendships/status'
+export const API_FRIENDSHIPS_PATCH_STATUS =
+  'http://localhost:3000/friendships/status'
 
 //create chats
 export const API_CHATS_POST_CREATE = 'http://localhost:3000/chats'
@@ -41,7 +46,8 @@ export const API_CHATS_GET_SEARCH = 'http://localhost:3000/chats/search'
 export const API_CHATS_POST_SEARCH = 'http://localhost:3000/chats/search'
 
 // preview msg
-export const API_MESSAGES_POST_PREVIEW = 'http://localhost:3000/messages/preview'
+export const API_MESSAGES_POST_PREVIEW =
+  'http://localhost:3000/messages/preview'
 
 //messages
 export const API_MESSAGES_POST_CREATE = 'http://localhost:3000/messages'
--- chatty_client/src/pages/friendship.tsx
+++ chatty_client/src/pages/friendship.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useAuth } from '../hooks/useAuth'
 import {
+  getFriendships,
   getFriendsReqList,
   updateFriendshipsStatus
 } from '../services/friendshipService'
@@ -8,7 +9,7 @@ import Card from '../ui/card'
 import Search from '../ui/search'
 import { getUsersProfile } from '../services/profileService'
 
-interface FriendshipsProfileProps {
+export interface FriendshipsProfileProps {
   owner: string
   nickname: string
   avatar: string
@@ -16,7 +17,15 @@ interface FriendshipsProfileProps {
 
 export default function Friendship() {
   const { user } = useAuth()
+  const [isSearch, setIsSearch] = useState<boolean>(false)
+  const [friendList, setFriendList] = useState<string[]>([]) //目前好友清單
+  const [reqIdList, setReqIdList] = useState<string[]>([]) //收到的申請
+  const [sendReqList, setSendReqList] = useState<string[]>([]) //送出的申請
+
   const [friendsProfile, setFriendsProfile] = useState<
+    FriendshipsProfileProps[] | null
+  >(null)
+  const [searchProfile, setSearchProfile] = useState<
     FriendshipsProfileProps[] | null
   >(null)
   const [isNull, setIsNull] = useState<boolean>(false)
@@ -28,7 +37,9 @@ export default function Friendship() {
     try {
       if (!user) return null
       const usersId = await getFriendsReqList(user.id)
+      setReqIdList(usersId)
 
+      setFriendList(await getFriendships(user.id))
       if (!usersId) {
         setIsNull(true)
         return
@@ -50,37 +61,85 @@ export default function Friendship() {
     }
   }
 
-  const handleFriendshipStatus = async (requester: string, status: string) => {
+  const handleSendReq = async (requester: string, status: string) => {
+    //送出申請
+    console.log('handleSendReq', requester)
+
     try {
-      console.log('1', requester, user!.id, status)
-      const res = await updateFriendshipsStatus(requester, user!.id, status)
-      console.log('handleFriendshipStatus res', res)
+      await updateFriendshipsStatus(user!.id, requester, status)
+      setSendReqList([...sendReqList, requester])
     } catch (err) {
-      console.log('handleFriendshipStatus', err)
+      console.log(err)
     }
   }
-  console.log('friendsProfile', friendsProfile)
+console.log('setFriendsProfile', friendsProfile)
+  const handleAcceptReq = async (requester: string, status: string) => {
+    //處理收到的申請
+    console.log('handleAcceptReq', requester)
+    try {
+      await updateFriendshipsStatus(requester, user!.id, status)
+      setFriendsProfile(friendsProfile!.filter(i=>i.owner !== requester))
+      setFriendList([...friendList, requester])
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    console.log('目前好友清單 friendList：', friendList)
+    console.log('收到的申請 reqIdList', reqIdList)
+    console.log('送出的申請 sendReqList', sendReqList)
+  }, [friendList, reqIdList, sendReqList])
+
   return (
-    <div className=' bg-slate-50 px-4 h-screen'>
-      <div className='my-3'>
+    <div className=' bg-slate-50 px-4 h-screen overflow-y-auto'>
+      <div className='py-3'>
         <div>
-          <Search />
+          <Search
+            setIsSearch={setIsSearch}
+            setSearchProfile={setSearchProfile}
+          />
         </div>
       </div>
       <div className='border-t border-gray-200 pt-3 space-y-2 '>
-        {isNull && (<div className='text-xs text-gray-400 text-center mt-3'> You are so lonely.. </div>)}
-        {friendsProfile?.map((user) => (
-          <div key={user.owner}>
-            <Card
-              friendship={{
-                onClick: handleFriendshipStatus,
-                id: user.owner,
-                name: user.nickname,
-                avatar: user.avatar
-              }}
-            />
+        {isNull && !isSearch && (
+          <div className='text-xs text-gray-400 text-center mt-3'>
+            You have no friends
           </div>
-        ))}
+        )}
+        {!isSearch &&
+          friendsProfile?.map((user) => (
+            <div key={user.owner}>
+              <Card
+                friendship={{
+                  onClick: handleAcceptReq,
+                  id: user.owner,
+                  name: user.nickname,
+                  avatar: user.avatar
+                }}
+              />
+            </div>
+          ))}
+
+        {isSearch &&
+          searchProfile
+            ?.filter(
+              (i) => i.owner !== user!.id && !friendList.includes(i.owner)
+            )
+            .map((user) => (
+              <div key={user.owner}>
+                <Card
+                  searchFriends={{
+                    onClick: handleSendReq,
+                    sendReqList,
+                    reqIdList,
+                    id: user.owner,
+                    name: user.nickname,
+                    avatar: user.avatar
+                  }}
+                />
+              </div>
+            ))}
       </div>
     </div>
   )
